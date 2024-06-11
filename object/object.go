8c3d40757441b443261d3cea838b0b5562be487a
janniksinz <j.sinz@gmx.de>
2024-06-11 20:58:35 +0200

    feat: add function object

--- object/object.go
+++ object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"monkey/ast"
+	"strings"
+)
 
 type ObjectType string
 
@@ -10,6 +15,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 type Object interface {
@@ -42,12 +48,33 @@ func (n *Null) Type() ObjectType         { return NULL_OBJ }
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (e *Error) Type() ObjectType        { return ERROR_OBJ }
 
+// env
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+
 // Inspect toString functions
 func (i *Integer) Inspect() string      { return fmt.Sprintf("%d", i.Value) }
 func (b *Boolean) Inspect() string      { return fmt.Sprintf("%t", b.Value) }
 func (n *Null) Inspect() string         { return "null" }
 func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
 func (e *Error) Inspect() string        { return "ERROR: " + e.Message }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+
+}
 
 // Environment
 func NewEnvironment() *Environment {
@@ -67,4 +94,10 @@ func (e *Environment) Get(name string) (Object, bool) {
 func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
+}
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
 }
