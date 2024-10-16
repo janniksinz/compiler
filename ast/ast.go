369d1ebb910e5bcaf78edbb75cf95c5591c6db73
janniksinz <j.sinz@gmx.de>
2024-10-16 21:58:15 +0200

    feat: add parsing hash literals

--- ast/ast.go
+++ ast/ast.go
@@ -118,6 +118,11 @@ type IndexExpression struct {
 	Index Expression  // the index of the object
 }
 
+type HashLiteral struct {
+	Token token.Token               // the '{' token
+	Pairs map[Expression]Expression // Go map of expressions
+}
+
 // Interface methods for
 //	- ReturnStatement
 //	- LetStatement
@@ -177,6 +182,9 @@ func (al *ArrayLiteral) TokenLiteral() string { return al.Token.Literal }
 
 func (ie *IndexExpression) expressionNode()      {}
 func (ie *IndexExpression) TokenLiteral() string { return ie.Token.Literal }
+
+func (hl *HashLiteral) expressionNode()      {}
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
 
 // String implementations for
 //   - Program
@@ -338,6 +346,21 @@ func (ie *IndexExpression) String() string {
 	out.WriteString("[")
 	out.WriteString(ie.Index.String())
 	out.WriteString("])")
+
+	return out.String()
+}
+
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
 
 	return out.String()
 }
