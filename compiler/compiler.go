6c20432aa9745b3c7b90e41c71eb64e7e44d3b9d
janniksinz <j.sinz@gmx.de>
2025-06-18 14:47:33 +0200

    feat: comp arrays

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -221,6 +221,16 @@ func (c *Compiler) Compile(node ast.Node) error {
 	case *ast.StringLiteral:
 		str := &object.String{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(str))
+
+	case *ast.ArrayLiteral:
+		for i, el := range node.Elements {
+			err := c.Compile(el)
+			if err != nil {
+				return fmt.Errorf("Compile(): array element %d coudn't compile %v", i, err)
+			}
+		}
+
+		c.emit(code.OpArray, len(node.Elements))
 	}
 	return nil
 }
