ec086e37bb6e11d757bfd70e66118c20fdfffddb
janniksinz <j.sinz@gmx.de>
2026-02-15 22:20:08 +0100

    feat(code): local bindings

--- code/code.go
+++ code/code.go
@@ -41,6 +41,8 @@ const (
 	OpCall
 	OpReturnValue
 	OpReturn
+	OpGetLocal
+	OpSetLocal
 )
 
 // maping opcode definitions
@@ -116,6 +118,12 @@ var definitions = map[Opcode]*Definition{
 	// +-------------+---------+---------+
 	OpSetGlobal: {"OpSetGlobal", []int{2}},
 
+	OpGetLocal: {"OpGetLocal", []int{1}},
+	// +------------+---------+---------+
+	// | OpSetLocal | 1 byte integer    |
+	// +------------+---------+---------+
+	OpSetLocal: {"OpSetLocal", []int{1}},
+
 	OpArray: {"OpArray", []int{2}},
 	// +---------+------------------+
 	// | OpArray | N (array length) |
@@ -176,6 +184,8 @@ func Make(op Opcode, operands ...int) []byte { // (opcode, int offset (location)
 		switch width {
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		case 1:
+			instruction[offset] = byte(o)
 		}
 		// first offset is 1 (opcode), increase offset by operand width
 		offset += width
@@ -235,6 +245,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 		switch width {
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		}
 
 		offset += width
@@ -245,4 +257,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 func ReadUint16(ins Instructions) uint16 {
 	return binary.BigEndian.Uint16(ins)
+}
+
+func ReadUint8(ins Instructions) uint8 {
+	return uint8(ins[0])
 }
--- code/code_test.go
+++ code/code_test.go
@@ -12,6 +12,8 @@ func TestMake(t *testing.T) {
 		// we expect a byte array []byte holding 3 bytes
 		// 1 - opcode (OpConstant); 2&3 - big endian encoding of 65534 (most significant comes first)
 		{OpAdd, []int{}, []byte{byte(OpAdd)}},
+		{OpGetLocal, []int{255}, []byte{byte(OpGetLocal), 255}},
+		{OpSetLocal, []int{255}, []byte{byte(OpSetLocal), 255}},
 	}
 
 	for _, tt := range tests {
@@ -39,13 +41,15 @@ func TestMake(t *testing.T) {
 func TestInstructionsString(t *testing.T) {
 	instructions := []Instructions{
 		Make(OpAdd),
+		Make(OpGetLocal, 1),
 		Make(OpConstant, 2),
 		Make(OpConstant, 65535),
 	}
 
 	expected := `0000 OpAdd
-0001 OpConstant 2
-0004 OpConstant 65535
+0001 OpGetLocal 1
+0003 OpConstant 2
+0006 OpConstant 65535
 `
 
 	concatted := Instructions{}
@@ -68,6 +72,7 @@ func TestReadOperands(t *testing.T) {
 		bytesRead int
 	}{
 		{OpConstant, []int{65535}, 2},
+		{OpGetLocal, []int{255}, 1},
 	}
 
 	for _, tt := range tests {
